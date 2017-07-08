b7ccecfc0006fbefae8da78449891545d14f8a21
Andrey V.Kosteltsev <kx@radix.pro>
2017-07-08 14:52:22 +0000

    libc, libm doc patch just to correct building
    git-svn-id: https://svn.radix.pro/svn/toolchains-arc/trunk@9 d6251417-76cf-4c88-927c-a0ac0606e556

---
+++ sources/newlib/create-2016.09.17-doc-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=2016.09.17
+
+tar --files-from=file.list -xJvf ../newlib-arc-$VERSION.tar.xz
+mv newlib-arc-$VERSION newlib-arc-$VERSION-orig
+
+cp -rf ./newlib-arc-$VERSION-new ./newlib-arc-$VERSION
+
+diff -b --unified -Nr  newlib-arc-$VERSION-orig  newlib-arc-$VERSION > newlib-arc-$VERSION-doc.patch
+
+mv newlib-arc-$VERSION-doc.patch ../patches
+
+rm -rf ./newlib-arc-$VERSION
+rm -rf ./newlib-arc-$VERSION-orig
